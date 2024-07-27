eb931175170240470e42661ec2b61f6a8a755585
Ankit Panwar <ankitpanwar212585@gmail.com>
2024-07-27 09:37:23 +0530

    all update done

--- Router/auth.js
+++ Router/auth.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const User = require("../model/User");
+
+const cryptoJS = require("crypto-js"); //there is the outer package
+const { route } = require("./user");
 // Register
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: cryptoJS.AES.encrypt(req.body.password, "panwar"),
   });
   try {
     const savedUser = await newUser.save();
@@ -15,6 +18,27 @@ router.post("/register", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
+  }
+});
+
+// Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("wrong candidate");
+    const hashedPassword = cryptoJS.AES.decrypt(
+      user.password,
+      "panwar"
+    );
+    const password = hashedPassword.toString(cryptoJS.enc.Utf8);
+
+    password !== req.body.password &&
+      res.status(401).json("wrong candidate");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.send(500).json(err);
   }
 });
 
